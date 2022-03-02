1bdadbfac7fb65a429db7c5fc0912e486eef269b
Victor Senna <vhsenna@gmail.com>
2022-03-01 21:52:14 -0300

    Create item

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Task
 
 class TaskList(ListView):
@@ -9,3 +11,9 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task_detail'
+
+class TaskCreate(CreateView):
+    model = Task
+    template_name = 'base/task_create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('task_list')
