559500bce6880aa313b12bcf10ac9e09b29b0d31
Victor Senna <vhsenna@gmail.com>
2022-03-01 21:00:52 -0300

    Detail page

--- base/models.py
+++ base/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
