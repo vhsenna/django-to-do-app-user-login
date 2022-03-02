f49038209d2377e3ad49c71e8b2d9f6589e690ac
Victor Senna <vhsenna@gmail.com>
2022-03-02 01:26:22 -0300

    Delete item

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
-    path('', TaskList.as_view(), name='task-list'),
-    path('task/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
-    path('task-create/', TaskCreate.as_view(), name='task-create'),
-    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('', TaskList.as_view(), name='task_list'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task_detail'),
+    path('task-create/', TaskCreate.as_view(), name='task_create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task_update'),
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task_delete'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -20,4 +20,9 @@ class TaskCreate(CreateView):
 class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
+    success_url = reverse_lazy('task_list')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task_delete'
     success_url = reverse_lazy('task_list')
